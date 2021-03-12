2be52b3ce612e92469c3e921661bef9db409e098
rage875 <rage.875@gmail.com>
2021-03-12 10:32:03 -0600

    Limiter and bulkhead features

--- src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -17,14 +18,16 @@ public class CircuitBreakerController {
 
 	@GetMapping("/sample-api")
 	//@Retry(name="sample-api", fallbackMethod = "hardcodedResponse")
-	@CircuitBreaker(name="default", fallbackMethod="hardcodedResponse")
+	//@CircuitBreaker(name="default", fallbackMethod="hardcodedResponse")
+	@RateLimiter(name="default")
+	// 10s => 10000 calls to the sample api
 	public String sampleApi() {
 		logger.info("Sample API call received");
 		// This will fail
-		ResponseEntity<String> forEntity =
-				new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-		return forEntity.getBody();
-		//return "Sample API";
+//		ResponseEntity<String> forEntity =
+//				new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//		return forEntity.getBody();
+		return "Sample API";
 	}
 
 	public String hardcodedResponse(Exception ex){
